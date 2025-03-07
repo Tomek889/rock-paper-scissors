953bf7db4617b46de7afb7a02dcba40e9386b389
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-07 20:57:38 +0100

    Create game's logic

--- js/script.js
+++ js/script.js
@@ -23,7 +23,37 @@ function getHumanChoice() {
 
 
 function playRound(humanSelection, computerSelection) {
-    // todo
+    if (humanSelection === 'rock') {
+        if (computerChoice === 'rock') {
+            return 'Human choice: rock, computer choice: rock\nIt\'s a tie.';
+        } else if (computerChoice === 'scissors') {
+            humanScore++;
+            return 'Human choice: rock, computer choice: scissors\nYou\'ve won!';
+        } else {
+            computerScore++;
+            return 'Human choice: rock, computer choice: paper\nYou\'ve lost.';
+        }
+    } else if (humanSelection === 'paper') {
+        if (computerChoice === 'rock') {
+            humanScore++;
+            return 'Human choice: paper, computer choice: rock\nYou\'ve won!';
+        } else if (computerChoice === 'scissors') {
+            computerScore++;
+            return 'Human choice: paper, computer choice: scissors\nYou\'ve lost.';
+        } else {
+            return 'Human choice: paper, computer choice: paper\nIt\'s a tie.';
+        }
+    } else {
+        if (computerChoice === 'rock') {
+            computerScore++;
+            return 'Human choice: scissors, computer choice: rock\nYou\'ve lost.';
+        } else if (computerChoice === 'scissors') {
+            return 'Human choice: scissors, computer choice: scissors\nIt\'s a tie.';
+        } else {
+            humanScore++;
+            return 'Human choice: scissors, computer choice: paper\nYou\'ve won!';
+        }
+    }
 }
 
 
